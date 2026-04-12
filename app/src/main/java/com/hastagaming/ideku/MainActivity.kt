8a98f1c04e03f064ed7d9fed862cabb7f30da881
hastagaming <rip.hastagaming@gmail.com>
2026-04-12 12:18:17 +0700

    fixing the service issues

--- app/src/main/java/com/hastagaming/ideku/MainActivity.kt
+++ app/src/main/java/com/hastagaming/ideku/MainActivity.kt
@@ -12,7 +12,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
